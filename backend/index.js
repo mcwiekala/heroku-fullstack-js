4e620a7a6bf5e67fc7ea1063dd4e5fcff2c2a060
Cwiekala Michal <cwiekala.michal@gmail.com>
2022-06-02 00:29:51 +0200

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const cors = require('cors')
 dotenv.config();
 
 const app = express();
+
+app.use(cors())
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
@@ -11,7 +13,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/message', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
   res.send('HELLO WORLD!');
 });
 
