de60c863ef1abdbdae61c6b15a86dca0920b8b9c
Cwiekala Michal <cwiekala.michal@gmail.com>
2022-06-03 23:26:35 +0200

    fix

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This application is a template to work with React and Node.
 
 https://heroku-test-195.herokuapp.com/
+
+For logs:
+```
+heroku logs --tail --app heroku-test-195
+```
--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -1,9 +1,9 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 
-const backendClient = axios.create({
-  baseURL: 'http://localhost:8000/api/'
-});
+// const backendClient = axios.create({
+//   baseURL: 'http://localhost:8000/api/'
+// });
 
 const Message = () => {
   const [message, setMessage] = useState(false)
@@ -11,7 +11,7 @@ const Message = () => {
   useEffect(() => {
     const fetchMessage = async () => {
       try{
-      const response = await backendClient.get("message");
+      const response = await axios.get("/api/message");
       console.log(response.data)
       setMessage(response.data)
       } catch (e) {
